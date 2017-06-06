d90cd8d481d7e329d6c868446dcfe95007301658
alagoro <alberto@alagoro.com>
2017-06-06 20:11:19 +0200

    corregir camino v.2

--- controller/ctm_customize.php
+++ controller/ctm_customize.php
@@ -26,7 +26,7 @@ class ctm_customize extends fs_controller {
 
         if (isset($files['favicon'])) {
             $fichero = $files['favicon'];
-            $filename = tempnam('plugins/customizer/files', 'icono' . '_');
+            $filename = tempnam(__DIR__.'/../files', 'icono' . '_');
             move_uploaded_file($fichero['tmp_name'], $filename);
 
             if (file_exists($filename)) {
